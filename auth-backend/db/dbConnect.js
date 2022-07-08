57b00eb4427b327f6f6039f892de100673ec2e26
J <jeremy.stegall77@gmail.com>
2022-07-08 19:38:51 -0400

    Create db/dbConnect.js

--- auth-backend/db/dbConnect.js
+++ auth-backend/db/dbConnect.js
@@ -3,7 +3,19 @@ require('dotenv').config();
 
 
 async function dbConnect() {
-    
+    mongoose
+        .connect(
+            process.env.DB_URL,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+            }
+        ).then(() => {
+            console.log("Successfully connected to MongoDB Atlas!");
+        }).catch((error) => {
+            console.log("Unable to connect to MongoDB Atlas!");
+        });
 }
 
 module.exports = dbConnect;
